fa20a49279721072d316720542f6bd06ade7a8f8
IliaTetin <121325844+IliaTetin@users.noreply.github.com>
2023-01-21 11:29:24 +0800

    Update 58. Length of Last Word.py

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -1,6 +1,10 @@
 # Given a string s consisting of words and spaces, return the length of the last word in the string.
 # A word is a maximal substring consisting of non-space characters only.
 
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+
 # Input: s = "   fly me   to   the moon  "
 # Output: 4
 # Explanation: The last word is "moon" with length 4.
