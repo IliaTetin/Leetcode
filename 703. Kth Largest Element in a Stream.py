4cbac94aecd115a559136401a0b23545c5719b48
IliaTetin <121325844+IliaTetin@users.noreply.github.com>
2024-10-12 18:09:07 +0800

    Create 703. Kth Largest Element in a Stream.py

---
+++ 703. Kth Largest Element in a Stream.py
@@ -0,0 +1,17 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.h = []
+        for i in nums:
+            heapq.heappush(self.h, i)
+            if len(self.h) > k:
+                heapq.heappop(self.h)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.h, val)
+        if len(self.h) > self.k:
+                heapq.heappop(self.h)
+
+        return self.h[0]
+        
